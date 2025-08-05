bc0f044b955a70a8f32bc25e81991299c8847fd5
Mike Olson <mike@madtownmining.com>
2025-08-05 11:41:03 -0500

    Fix Anchor deserialization and all tests passing.

--- programs/capstone/src/tests.rs
+++ programs/capstone/src/tests.rs
@@ -1,3 +1,4 @@
+use anchor_lang::AccountDeserialize;
 use solana_kite::{
     get_pda_and_bump, get_token_account_balance, seeds, send_transaction_from_instructions,
 };
@@ -6,6 +7,7 @@ use solana_signer::Signer;
 use crate::test_helpers::{
     build_initialize_accounts, build_initialize_instruction, generate_id, TestHarness, USDC_TOKEN,
 };
+use crate::MeditationPlan;
 
 #[test]
 fn test_initialize_succeeds() {
@@ -60,24 +62,20 @@ fn test_initialize_succeeds() {
     let raw_plan_account = harness.svm.get_account(&meditation_plan).unwrap();
     assert_eq!(raw_plan_account.owner, harness.program_id);
 
-    // println!("data length {:?}", raw_plan_account.data.len());
-    // println!(
-    //     "MP discriminator length {:?}",
-    //     MeditationPlan::DISCRIMINATOR.len()
-    // );
-    // let plan = MeditationPlan::try_from_slice(&raw_plan_account.data.as_slice()).unwrap();
-    // assert_eq!(plan.attestations.len(), 0);
-    // assert!(plan.bump > 0);
-    // assert_eq!(plan.commitment_stake, commitment_stake);
-    // assert_eq!(plan.daily_frequency, daily_frequency);
-    // assert_eq!(plan.duration_minutes, duration_minutes);
-    // assert_eq!(plan.id, id);
-    // assert_eq!(plan.is_active, false);
-    // assert_eq!(plan.is_completed, false);
-    // assert_eq!(plan.number_of_days, number_of_days);
-    // assert_eq!(plan.owner, harness.alice.pubkey());
-    // assert_eq!(plan.penalties, 0);
-    // assert_eq!(plan.rewards, 0);
-    // assert_eq!(plan.start_at, 0);
-    // assert_eq!(plan.end_at, number_of_days as i64 * 24 * 60 * 60);
+    let plan = MeditationPlan::try_deserialize(&mut raw_plan_account.data.as_slice())
+        .expect("Anchor deserialize should succeed");
+    assert_eq!(plan.attestations.len(), 0);
+    assert!(plan.bump > 0);
+    assert_eq!(plan.commitment_stake, commitment_stake);
+    assert_eq!(plan.daily_frequency, daily_frequency);
+    assert_eq!(plan.duration_minutes, duration_minutes);
+    assert_eq!(plan.id, id);
+    assert_eq!(plan.is_active, false);
+    assert_eq!(plan.is_completed, false);
+    assert_eq!(plan.number_of_days, number_of_days);
+    assert_eq!(plan.owner, harness.alice.pubkey());
+    assert_eq!(plan.penalties, 0);
+    assert_eq!(plan.rewards, 0);
+    assert_eq!(plan.start_at, 0);
+    assert_eq!(plan.end_at, number_of_days as i64 * 24 * 60 * 60);
 }
